db86fe8d165abb572e06a53bee18db7b787141f8
NoBugsHere <eugene200020@gmail.com>
2019-05-28 06:38:31 +0300

    Sayings added

---
+++ commands/sayings.py
@@ -0,0 +1,93 @@
+import system.command_system
+import vk_io.vk_api
+import re
+import random
+
+
+def sayings():
+
+    def clean_output(text):
+        text = re.sub('{.+?}', '', text)
+        return text
+
+    def generator(first_halves, second_halves):
+
+        # codes = ('пов,2-л',
+        #          'нп=непрош,ед,изъяв,3-л',
+        #          'мн,изъяв,1-л',
+        #          'ед,кр,муж',
+        #          'нп=непрош,ед,изъяв,3-л,сов',
+        #          'нп=непрош,ед,изъяв,3-л,несов',
+        #          'прош,мн,изъяв,сов,пе',
+        #          'прош,мн,изъяв,сов,пе',
+        #          'нп=прош,ед,изъяв,сред,сов',
+        #          '=V,пе=инф,несов')
+
+        with open('sayings.txt', encoding='utf-8') as f:
+            originals = f.read()
+        codes = {
+            'imperative': '{.+пов.+}',
+            '1pl': '{.+V.+мн,изъяв,1-л}',
+            '3sg': '{.+V.+ед,изъяв,3-л}',
+            '2sg': '{.+V.+ед,изъяв,2-л}'
+        }
+        beginnings = {}
+        endings = {}
+        for first_half in first_halves:
+            beginnings[first_half] = []
+        # Записываем коды половин пословиц
+        for i, second_half in enumerate(second_halves):
+            for code in list(codes.keys()):
+                if re.search(codes[code], second_half):
+                    if code not in endings:
+                        endings[code] = []
+                    endings[code].append(second_half)
+                    beginnings[first_halves[i]].append(code)
+        for i, second_half in enumerate(second_halves):
+            if re.search('{.+инф.+}', second_half) and not re.search('{.+изъяв.+}', second_half) and not re.search(
+                    '{.+пов.+}', second_half):
+                if 'infinitive' not in endings:
+                    endings['infinitive'] = []
+                endings['infinitive'].append(second_half)
+                beginnings[first_halves[i]].append('infinitive')
+        for i, second_half in enumerate(second_halves):
+            if re.search('{.+им.+}', second_half) and not re.search('{.+V.+}', second_half):
+                if 'nominative' not in endings:
+                    endings['nominative'] = []
+                endings['nominative'].append(second_half)
+                beginnings[first_halves[i]].append('nominative')
+        for i, second_half in enumerate(second_halves):
+            if not re.search('{.+им.+}', second_half) and not re.search('{.+V.+}', second_half):
+                if 'oblique' not in endings:
+                    endings['oblique'] = []
+                endings['oblique'].append(second_half)
+                beginnings[first_halves[i]].append('oblique')
+        beginning = ''
+        ending = ''
+        saying = ''
+        while beginning == '' or ending == '':
+            beginning = random.choice(list(beginnings.keys()))
+            if beginnings[beginning]:
+                ending = random.choice(endings[beginnings[beginning][0]])
+            code = beginnings[beginning]
+            saying = clean_output('{beginning} {ending}'.format(beginning=beginning, ending=ending))
+            if saying in originals:
+                beginning = ''
+                ending = ''
+                continue
+        output = '{code}\n{saying}.'.format(code=code, saying=saying)
+        return output
+
+    with open('first_halves.txt', encoding='utf-8') as f:
+        first_halves = f.read().split('\n')
+    with open('second_halves.txt', encoding='utf-8') as f:
+        second_halves = f.read().split('\n')
+    final_output = generator(first_halves, second_halves)
+    return final_output, ''
+
+
+sayings_command = system.command_system.Command()
+
+sayings_command.key = 'пословица'
+sayings_command.description = 'Пришлю тебе твою любимую пословицу)'
+sayings_command.process = sayings
--- sayings.py
+++ sayings.py
@@ -6,40 +6,47 @@ import random
 #     originals = f.read()
 
 
-codes = ('пов,2-л',
-         'нп=непрош,ед,изъяв,3-л',
-         'мн,изъяв,1-л',
-         'ед,кр,муж',
-         'нп=непрош,ед,изъяв,3-л,сов',
-         'нп=непрош,ед,изъяв,3-л,несов',
-         'прош,мн,изъяв,сов,пе',
-         'прош,мн,изъяв,сов,пе',
-         'нп=прош,ед,изъяв,сред,сов',
-         '=V,пе=инф,несов')
+# codes = ('пов,2-л',
+#          'нп=непрош,ед,изъяв,3-л',
+#          'мн,изъяв,1-л',
+#          'ед,кр,муж',
+#          'нп=непрош,ед,изъяв,3-л,сов',
+#          'нп=непрош,ед,изъяв,3-л,несов',
+#          'прош,мн,изъяв,сов,пе',
+#          'прош,мн,изъяв,сов,пе',
+#          'нп=прош,ед,изъяв,сред,сов',
+#          '=V,пе=инф,несов')
 
 def clean_output(saying):
     saying = re.sub('{.+?}', '', saying)
     return saying
 
 
-
 def generator(first_halves, second_halves):
+    codes = {
+        'imperative': '{.+пов.+}',
+        '1pl': '{.+V.+мн,изъяв,1-л}',
+        '3sg': '{.+V.+ед,изъяв,3-л}',
+        '2sg': '{.+V.+ед,изъяв,2-л}'
+    }
     beginnings = {}
     endings = {}
     for first_half in first_halves:
         beginnings[first_half] = []
+    # Записываем коды половин пословиц
     for i, second_half in enumerate(second_halves):
-        if re.search('{.+пов.+}', second_half):
-            if 'imperative' not in endings:
-                endings['imperative'] = []
-            endings['imperative'].append(second_half)
-            beginnings[first_halves[i]].append('imperative')
+        for code in list(codes.keys()):
+            if re.search(codes[code], second_half):
+                if code not in endings:
+                    endings[code] = []
+                endings[code].append(second_half)
+                beginnings[first_halves[i]].append(code)
     for i, second_half in enumerate(second_halves):
-        if re.search('{.+V.+мн.+1-л.+}', second_half):
-            if '1pl' not in endings:
-                endings['1pl'] = []
-            endings['1pl'].append(second_half)
-            beginnings[first_halves[i]].append('1pl')
+        if re.search('{.+инф.+}', second_half) and not re.search('{.+изъяв.+}', second_half) and not re.search('{.+пов.+}', second_half):
+            if 'infinitive' not in endings:
+                endings['infinitive'] = []
+            endings['infinitive'].append(second_half)
+            beginnings[first_halves[i]].append('infinitive')
     for i, second_half in enumerate(second_halves):
         if re.search('{.+им.+}', second_half) and not re.search('{.+V.+}', second_half):
             if 'nominative' not in endings:
@@ -47,11 +54,11 @@ def generator(first_halves, second_halves):
             endings['nominative'].append(second_half)
             beginnings[first_halves[i]].append('nominative')
     for i, second_half in enumerate(second_halves):
-        if re.search('{.+инф.+}', second_half):
-            if 'infinitive' not in endings:
-                endings['infinitive'] = []
-            endings['infinitive'].append(second_half)
-            beginnings[first_halves[i]].append('infinitive')
+        if not re.search('{.+им.+}', second_half) and not re.search('{.+V.+}', second_half):
+            if 'oblique' not in endings:
+                endings['oblique'] = []
+            endings['oblique'].append(second_half)
+            beginnings[first_halves[i]].append('oblique')
     for i in range(100):
         beginning = ''
         ending = ''
@@ -61,6 +68,7 @@ def generator(first_halves, second_halves):
             beginning = random.choice(list(beginnings.keys()))
             if beginnings[beginning]:
                 ending = random.choice(endings[beginnings[beginning][0]])
+        print(beginnings[beginning])
         beginning = clean_output(beginning)
         ending = clean_output(ending)
         print(beginning, ending)
@@ -82,6 +90,7 @@ def main():
     #     saying = clean_output(generator(first_halves, second_halves))
     # print(saying + '.')
     generator(first_halves, second_halves)
+
 
 if __name__ == '__main__':
     main()
