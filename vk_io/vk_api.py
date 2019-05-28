c85bb9ed121108dee7f85badc86803eb78f9ff3b
milesartemius <shveitsar215@gmail.com>
2019-05-28 05:22:07 +0300

    fix

--- vk_io/vk_api.py
+++ vk_io/vk_api.py
@@ -1,10 +1,11 @@
 import vk
 import vk_io.settings
 import random
+import sys
 
 session = vk.Session(vk_io.settings.token)
 api = vk.API(session, v=5.95)
 
 
 def send_message(user_id, token, message, attachment=""):
-    api.messages.send(access_token=token, random_id=random.randint, user_id=str(user_id), message=message, attachment=attachment)
+    api.messages.send(access_token=token, random_id=random.randint(0, sys.maxsize), user_id=str(user_id), message=message, attachment=attachment)
