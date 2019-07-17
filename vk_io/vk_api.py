279e5fa9fc46667ac5b286385bf4a0e25ea43f99
milesartemius <shveitsar215@gmail.com>
2019-07-17 13:20:01 +0300

    telegram intro and content stealing

--- vk_io/vk_api.py
+++ vk_io/vk_api.py
@@ -9,14 +9,15 @@ class VkApi(system.api.Api):
     def __init__(self):
         super().__init__()
         self.session = vk.Session(vk_io.settings.token)
-        self.api = vk.API(self.session, v=5.95)
+        self.api = vk.API(self.session)
 
     def get_user_id(self, message):
         peer_id = message['object']['from_id']
-        if peer_id < 2000000000:
+        return peer_id
+        '''if peer_id < 2000000000:
             return peer_id
         else:
-            return peer_id - 2000000000
+            return peer_id - 2000000000'''
 
     def send_message(self, message, user_id, attachment='', token=vk_io.settings.token):
         self.api.messages.send(access_token=token, peer_id=str(user_id), random_id=random.randint(0, sys.maxsize), message=message, attachment=attachment)
