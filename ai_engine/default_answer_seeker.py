7afb6ef12e37c8ed5d70dd9bfe73550d39075e88
milesartemius <shveitsar215@gmail.com>
2019-09-18 00:08:09 +0300

    yandex search example added

--- ai_engine/default_answer_seeker.py
+++ ai_engine/default_answer_seeker.py
@@ -1,11 +1,6 @@
 import requests
 
-address = "https://yandex.ru/search/?"
-region = "lr=2&"
-query_start = "text="
-
 
 def mk_request(query):
-    q = address + region + query_start + "%20".join(query.split())
-    response = requests.get(q)
+    response = requests.get(query)
     return response.text
--- commands/yarik.py
+++ commands/yarik.py
@@ -16,4 +16,12 @@ def search(text):
         query = text[0]
     else:
         query = 'pneis'
-    return str(ai_engine.default_answer_seeker.mk_request(query)), ''
+
+    address = "https://yandex.ru/search/?"
+    region = "lr=2&"
+    query_start = "text="
+    q = address + region + query_start + "%20".join(query.split())
+
+    html_request = ai_engine.default_answer_seeker.mk_request(q)  # это html запроса в яндексе. используй его с умом. дляя получения другого html по любой другой ссылке - используй тот же метод.
+
+    return str(), ''
